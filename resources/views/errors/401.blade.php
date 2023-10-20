67a68c5ed77f2b4e28c3eb125ffb03110e34c56f
Vinod Sarode <vs10201020@gmail.com>
2023-10-20 15:09:27 +0530

    sm

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,5 +1,11 @@
-@extends('errors::minimal')
+<x-layout.auth>  
 
-@section('title', __('Unauthorized'))
-@section('code', '401')
-@section('message', __('Unauthorized'))
+<div class="flex justify-center items-center min-h-screen bg-gradient-to-t from-[#c39be3] to-[#f2eafa]">
+        <div class="text-center p-5 font-semibold">
+            <h2 class="text-[50px] md:text-[80px] leading-none mb-8 font-bold">Error 401</h2>
+            <h4 class="mb-5 font-semibold text-xl sm:text-5xl text-primary">Ooops!</h4>
+            <p class="text-base">Unauthorized!</p>
+            <a href="/" class="btn btn-primary mt-10 w-max mx-auto">Home</a>
+        </div>
+    </div>
+</x-layout.auth>  
