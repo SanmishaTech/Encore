3392e7aaa23ba3d3869e0b76764c67b685fac0a6
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-08-05 13:31:48 +0530

    CHEMIST

--- app/Imports/ImportChemists.php
+++ app/Imports/ImportChemists.php
@@ -2,13 +2,14 @@
 namespace App\Imports;
 use App\Models\Chemist;
 use App\Models\Employee;
-use Illuminate\Support\Facades\DB;
+use App\Models\Territory;
 
-use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\Importable;
-use Maatwebsite\Excel\Concerns\WithValidation;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 
@@ -39,10 +40,16 @@ class ImportChemists implements ToModel, WithHeadingRow, WithValidation, WithBat
         $employee = DB::table('employees')->where('employee_code', $row['employee_code'])->first();
         $territory = DB::table('territories')->where('name', $row['territory'])->first();
 
+        if(!$territory){
+            $territory = new Territory();
+            $territory->name = $row['territory'];
+            $territory->save();
+        }
+
         if(!$employee || !$territory) {
             echo "<pre>";
             echo "Employee <br />"; print_r($employee); echo "<hr/>";
-            echo "Territory <br />"; print_r($territory); echo "<hr/>";
+             echo "Territory <br />"; print_r($territory); echo "<hr/>";
             // echo "Data <br />"; print_r($row);
             echo "</pre>";
             // exit;
